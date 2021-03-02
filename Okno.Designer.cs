1670fa0cc0c00f29f6e48d85c0d159983e51bf8f
adignaczak@gmail.com <adignaczak@gmail.com>
2021-03-02 19:28:27 +0100

    Dodaj pliki projektów.

---
+++ Okno.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace WF_SignUp
+{
+    partial class Okno
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TBLogin = new System.Windows.Forms.TextBox();
+            this.TBPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TBRPassword = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TBRLogin = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.DTPBirthday = new System.Windows.Forms.DateTimePicker();
+            this.ButtonLogin = new System.Windows.Forms.Button();
+            this.ButtonSignIn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(341, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 64);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Witaj";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(191, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Login:";
+            // 
+            // TBLogin
+            // 
+            this.TBLogin.Location = new System.Drawing.Point(191, 186);
+            this.TBLogin.Name = "TBLogin";
+            this.TBLogin.Size = new System.Drawing.Size(149, 23);
+            this.TBLogin.TabIndex = 2;
+            // 
+            // TBPassword
+            // 
+            this.TBPassword.Location = new System.Drawing.Point(191, 236);
+            this.TBPassword.Name = "TBPassword";
+            this.TBPassword.Size = new System.Drawing.Size(149, 23);
+            this.TBPassword.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(191, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Hasło:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(221, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 21);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Logowanie";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(502, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 21);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Rejestracja";
+            // 
+            // TBRPassword
+            // 
+            this.TBRPassword.Location = new System.Drawing.Point(472, 236);
+            this.TBRPassword.Name = "TBRPassword";
+            this.TBRPassword.Size = new System.Drawing.Size(149, 23);
+            this.TBRPassword.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(472, 217);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Hasło:";
+            // 
+            // TBRLogin
+            // 
+            this.TBRLogin.Location = new System.Drawing.Point(472, 186);
+            this.TBRLogin.Name = "TBRLogin";
+            this.TBRLogin.Size = new System.Drawing.Size(149, 23);
+            this.TBRLogin.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(472, 167);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Login:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(472, 262);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 15);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Data Urodzenia:";
+            // 
+            // DTPBirthday
+            // 
+            this.DTPBirthday.Location = new System.Drawing.Point(472, 281);
+            this.DTPBirthday.Name = "DTPBirthday";
+            this.DTPBirthday.Size = new System.Drawing.Size(200, 23);
+            this.DTPBirthday.TabIndex = 12;
+            // 
+            // ButtonLogin
+            // 
+            this.ButtonLogin.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ButtonLogin.Location = new System.Drawing.Point(230, 281);
+            this.ButtonLogin.Name = "ButtonLogin";
+            this.ButtonLogin.Size = new System.Drawing.Size(75, 23);
+            this.ButtonLogin.TabIndex = 13;
+            this.ButtonLogin.Text = "Zaloguj";
+            this.ButtonLogin.UseVisualStyleBackColor = true;
+            // 
+            // ButtonSignIn
+            // 
+            this.ButtonSignIn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ButtonSignIn.Location = new System.Drawing.Point(502, 319);
+            this.ButtonSignIn.Name = "ButtonSignIn";
+            this.ButtonSignIn.Size = new System.Drawing.Size(136, 23);
+            this.ButtonSignIn.TabIndex = 14;
+            this.ButtonSignIn.Text = "Utwórz użytkownika";
+            this.ButtonSignIn.UseVisualStyleBackColor = true;
+            // 
+            // Okno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ButtonSignIn);
+            this.Controls.Add(this.ButtonLogin);
+            this.Controls.Add(this.DTPBirthday);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TBRPassword);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TBRLogin);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TBPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TBLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Okno";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TBLogin;
+        private System.Windows.Forms.TextBox TBPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TBRPassword;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TBRLogin;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DateTimePicker DTPBirthday;
+        private System.Windows.Forms.Button ButtonLogin;
+        private System.Windows.Forms.Button ButtonSignIn;
+    }
+}
+
---
+++ Okno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_SignUp
+{
+    public partial class Okno : Form
+    {
+        public Okno()
+        {
+            InitializeComponent();
+            this.Text = "Logowanie";
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+       
+    }
+}
