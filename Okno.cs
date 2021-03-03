5e4aac6debe098884c7a11d7c93541c4168d7c5c
adignaczak@gmail.com <adignaczak@gmail.com>
2021-03-03 18:56:38 +0100

    Sprawdź logowanie

--- Okno.cs
+++ Okno.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -24,75 +25,83 @@ namespace WF_SignUp
 
         private void ButtonLogin_Click(object sender, EventArgs e)
         {
+            string dbConnect = @"Data Source =Users.db";
             //Tutaj trzeba dać sprawdzenie logowania itp
-            EmptyLogin();
-            EmptyPass();
-            MessageBox.Show("Zalogowano pomyślnie " + TBLogin.Text + " " + TBPassword.Text);
-        }
-
-        private void ButtonLogin_Click(object sender, EventArgs e)
-        {
-            //Tutaj trzeba dać sprawdzenie logowania itp
-            EmptyLogin();
-            EmptyPass();
-            MessageBox.Show("Zalogowano pomyślnie " + TBLogin.Text + " " + TBPassword.Text);
-        }
-
-        private void ButtonSignIn_Click(object sender, EventArgs e)
-        {
-            //Rejestracja
-            db.Add(new User { Login = TBRLogin.Text, Password = TBRPassword.Text, Birthday = DTPBirthday.Value }) ;
-
-        }
-        private void EmptyLogin()
-        {
             if (TBLogin.TextLength == 0)
             {
                 MessageBox.Show("Podaj login !");
             }
-           // else if(TBRLogin.Text == User. )
-           //  {
-           //      db.
-           // }
-        }
-        private void EmptyPass()
-        private void ButtonSignIn_Click(object sender, EventArgs e)
-        {
-            //Rejestracja
-            db.Add(new User { Login = TBRLogin.Text, Password = TBRPassword.Text, Birthday = DTPBirthday.Value }) ;
-
-        }
-        private void EmptyLogin()
-        {
-            if (TBLogin.TextLength == 0)
-            {
-                MessageBox.Show("Podaj login !");
-            }
-
-          
-           // else if(TBRLogin.Text == User. )
-            // {
-           //      db.Users.Where(b => b.Login == Users.Login);
-           //  }
-          //
-
-           // else if(TBRLogin.Text == User. )
-           //  {
-           //      db.
-           // }
-
-        }
-        private void EmptyPass()
-        {
-            
-            if(TBPassword.TextLength == 0)
+            else if (TBPassword.TextLength == 0)
             {
                 MessageBox.Show("Podaj hasło !");
+            }         
+            else
+            {
+                //Połaczenie z bazą danych
+                SqliteConnection sqliteCon = new SqliteConnection(dbConnect);
+                try 
+                {
+                    sqliteCon.Open();
+                    string Query = "SELECT * FROM Users WHERE Login='" + TBLogin.Text + " ' AND Password='" + TBPassword.Text + "' ";
+                    SqliteCommand sqlsearch = new SqliteCommand(Query, sqliteCon);
+
+                    sqlsearch.ExecuteNonQuery();
+                    SqliteDataReader data = sqlsearch.ExecuteReader();
+
+                    int count = 0;
+                    while(data.Read())
+                    {
+                        count++;
+                    }
+                    if(count == 1)
+                    {
+                        MessageBox.Show("Zalogowano");
+                    }
+                    if (count > 1)
+                    {
+                        MessageBox.Show("Login nie może być taki sam jak hasło spróbuj jeszcze raz");
+                    }
+                    if (count < 1)
+                    {
+                        MessageBox.Show("Zły login lub hasło");
+                    }
+                }
+
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+              
+            }
+        }
+    
+
+        private void ButtonSignIn_Click(object sender, EventArgs e)
+        {
+            //Rejestracja
+            if (TBRLogin.TextLength == 0)
+            {
+                MessageBox.Show("Podaj login");
+            }
+            else if (TBRPassword.TextLength == 0)
+            {
+                MessageBox.Show("Podaj hasło");
+            }
+            else if (TBRPassword.Text == TBRLogin.Text)
+            {
+                MessageBox.Show("Login nie może być taki sam jak hasło");
             }
             else
             {
+                MessageBox.Show("Wykonany");
+                db.Add(new User { Login = TBRLogin.Text, Password = TBRPassword.Text, Birthday = DTPBirthday.Value });
+                db.SaveChanges();
             }
         }
+        
+        
+      
+        
 
        
     }
